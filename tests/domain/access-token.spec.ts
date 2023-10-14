7730d2d036b7cc2fed2166f5e465a386215a1b32
adrianosantana <ans3@cin.ufpe.br>
2023-10-14 15:35:09 -0300

    ensure access token has a value

---
+++ tests/domain/access-token.spec.ts
@@ -0,0 +1,10 @@
+import { FacebookAccount } from '../../src/domain/models/facebook-accounts'
+import { AccessToken } from "../../src/domain/models"
+
+describe('Access Token', () => {
+
+  it('should create with a value', () => {
+    const sut = new AccessToken('any_value')
+    expect(sut).toEqual({ value: 'any_value'})
+  })
+})
