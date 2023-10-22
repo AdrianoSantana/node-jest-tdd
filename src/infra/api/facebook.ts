3ddb84682f3cfb26f622ae5efe5d6c46d2c55974
Usuario <ans3@cin.ufpe.br>
2023-10-22 13:39:54 -0300

    ensure facebookApi gets user info

--- src/infra/api/facebook.ts
+++ src/infra/api/facebook.ts
@@ -9,12 +9,28 @@ export class FacebookApi {
     private readonly clientSecret: string
   ) {}
   public async loadUser(params: LoadFacebookUserApi.Params): Promise<void> {
-    await this.httpGetClient.get({ 
+    const appToken = await this.httpGetClient.get({ 
       url: `${this.BASE_URL}/oauth/access_token`,
       params: {
         client_id: this.clientId,
         client_secret: this.clientSecret,
         grant_type: 'cliente_credentials'
+      }
+    })
+
+    const debugToken = await this.httpGetClient.get({
+      url: `${this.BASE_URL}/debug_token`,
+      params: {
+        access_token: appToken.access_token,
+        input_token: params.token
+      }
+    })
+
+    await this.httpGetClient.get({
+      url: `${this.BASE_URL}/${debugToken.data.user_id}`,
+      params: {
+        fields: ['id', 'name', 'email'].join(','),
+        access_token: params.token
       }
     })
   }
--- tests/infra/facebook-api.spec.ts
+++ tests/infra/facebook-api.spec.ts
@@ -16,18 +16,45 @@ describe('Facebook API', () => {
   })
 
   beforeEach(() => {
+    httpClient.get
+      .mockResolvedValueOnce({ access_token: 'any_app_token' })
+      .mockResolvedValueOnce({ data: { user_id: 'any_user_id'}})
+
     sut = new FacebookApi(httpClient, clientId, clientSecret)
   })
 
   it('Should get App Token', async () => {
 
-    const user = await sut.loadUser({ token: 'any_client_toker' })
+    const user = await sut.loadUser({ token: 'any_client_token' })
     expect(httpClient.get).toHaveBeenCalledWith({
       url: 'https://graph.facebook.com/oauth/access_token',
       params: {
         client_id: clientId,
         client_secret: clientSecret,
         grant_type: 'cliente_credentials'
+      }
+    })
+  })
+
+  it('Should get Debug Token', async () => {
+    await sut.loadUser({ token: 'any_client_token' })
+    expect(httpClient.get).toHaveBeenCalledWith({
+      url: 'https://graph.facebook.com/debug_token',
+      params: {
+        access_token: 'any_app_token',
+        input_token: 'any_client_token',
+      }
+    })
+  })
+
+  it('Should get user info', async () => {
+    await sut.loadUser({ token: 'any_client_token' })
+    
+    expect(httpClient.get).toHaveBeenCalledWith({
+      url: 'https://graph.facebook.com/any_user_id',
+      params: {
+        fields: 'id,name,email',
+        access_token: 'any_client_token',
       }
     })
   })
