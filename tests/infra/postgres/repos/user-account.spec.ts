f47ce73527680df9359a4c921f5c959942422a76
adrianosantana <ans3@cin.ufpe.br>
2023-11-08 19:04:12 -0300

    ensure pgUserAccountRepository loads an account if email exists

--- tests/infra/postgres/repos/user-account.spec.ts
+++ tests/infra/postgres/repos/user-account.spec.ts
@@ -1,41 +1,65 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm'
+import { Column, DataSource, Entity, PrimaryGeneratedColumn, Repository } from 'typeorm'
 import { LoadUserAccountRepository } from '../../../../src/data/contracts/repositories'
 import { newDb } from 'pg-mem'
 
 class PgAccountRepository implements LoadUserAccountRepository {
+    constructor(private readonly repository: Repository<PgUser> ) {}
     async load(params: LoadUserAccountRepository.Params): Promise<LoadUserAccountRepository.Result> {
-
+       const pgUser = await this.repository.findOne({ where: { email: params.email }})
+       if (pgUser) {
+           return {
+            id: pgUser.id.toString(),
+            name: pgUser.name ?? undefined
+           }
+       }
     }
 }
 
 @Entity({ name: 'usuarios'})
 export class PgUser {
     @PrimaryGeneratedColumn()
-    id: number
+    id!: number
+
+    @Column({  nullable: true, name: 'nome'})
+    name?: string
 
     @Column()
-    name: string
+    email!: string
 
-    @Column()
-    lastName: string
-
-    @Column()
-    age: number
+    @Column({ nullable: true, name: 'id_facebook'})
+    facebookId?: string
 }
 
 describe('PG User account repo', () => {
     describe('load', () => {
         it('Should return an account if email exists', async () => {
-            const db = newDb();
-            const connection = await db.adapters.createTypeormConnection({
+            const db = newDb({
+                autoCreateForeignKeyIndices: true
+            })
+             
+            db.public.registerFunction({
+                implementation: () => 'test',
+                name: 'current_database',
+            });
+
+            db.public.registerFunction({
+                implementation: () => 'test',
+                name: 'version',
+            })
+             
+            const dataSource: DataSource = await db.adapters.createTypeormDataSource({
                 type: 'postgres',
-                entities: []
+                entities: [PgUser]
             })
 
-            await connection.synchronize();
+            await dataSource.initialize()
+            await dataSource.synchronize()
+
+            const pgUserRepository = dataSource.getRepository(PgUser)
+            await pgUserRepository.save({ email: 'existing_email' })
 
 
-            const sut = new PgAccountRepository()
+            const sut = new PgAccountRepository(pgUserRepository)
             const account = await sut.load({ email: 'existing_email' })
             expect(account).toEqual({
                 id: '1'
